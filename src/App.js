57af6688da4d3d429f6dff1a1da66641b883b36e
kevin1547 <c110181143@nkust.edu.tw>
2022-03-20 22:23:41 +0800

    homework1+2

--- src/App.js
+++ src/App.js
@@ -25,33 +25,17 @@ function App() {
 
 export default App;
 */
-/*
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react'
-function App() {
-  const [number, setNumber] =useState(0)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>{number}</div>
-        <button onClick={()=>{setNumber(number+1)}}>加一</button>
-        <button onClick={()=>{setNumber(number-1)}}>減一</button>
-      </header>
-      
-    </div>
-  );
-}
-*/
 
 
 
 import Slider from './Work/Slider'
-
+import LoginWork from './components/LoginWork'
 function App() {
   return (
     <>
     <Slider />
+    <LoginWork />
+    
     </>
   );
 }
