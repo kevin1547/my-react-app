d68e71d11e7241ed92b3b8194048cffa5313a6ef
kevin1547 <c110181143@nkust.edu.tw>
2022-05-03 21:59:30 +0800

    mobx5/3

--- src/Work/CounterByMyself/MainFunc.js
+++ src/Work/CounterByMyself/MainFunc.js
@@ -15,141 +15,143 @@ export default function MainFunc() {
   return (
     <div className="main-in-counterbymyself">
       <div className="sec-in-counterbymyself">
-        <div className="input-space">{space}</div>
-      </div>
-      <div className="btn-in-counterbymyself">
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("7", setSpace, space);
-          }}
-        >
-          7
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("8", setSpace, space);
-          }}
-        >
-          8
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("9", setSpace, space);
-          }}
-        >
-          9
-        </button>
-        <button
-          className="button-in-mainfunc notNumber"
-          onClick={() => {
-            calculatingSigns("/", space, setSpace);
-          }}
-        >
-          /
-        </button>
-        <button className="button-in-mainfunc" onClick={clear}>
-          clear
-        </button>
-      </div>
-      <div className="btn-in-counterbymyself">
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("4", setSpace, space);
-          }}
-        >
-          4
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("5", setSpace, space);
-          }}
-        >
-          5
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("6", setSpace, space);
-          }}
-        >
-          6
-        </button>
-        <button
-          className="button-in-mainfunc notNumber"
-          onClick={() => {
-            calculatingSigns("*", space, setSpace);
-          }}
-        >
-          *
-        </button>
-      </div>
-      <div className="btn-in-counterbymyself">
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("1", setSpace, space);
-          }}
-        >
-          1
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("2", setSpace, space);
-          }}
-        >
-          2
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("3", setSpace, space);
-          }}
-        >
-          3
-        </button>
-        <button
-          className="button-in-mainfunc notNumber"
-          onClick={() => {
-            calculatingSigns("+", space, setSpace);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div className="btn-in-counterbymyself">
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            calculatingSigns(".", space, setSpace);
-          }}
-        >
-          .
-        </button>
-        <button
-          className="button-in-mainfunc"
-          onClick={() => {
-            btn("0", setSpace, space);
-          }}
-        >
-          0
-        </button>
-        <button className="button-in-mainfunc" onClick={equal}>
-          =
-        </button>
-        <button
-          className="button-in-mainfunc notNumber"
-          onClick={() => {
-            calculatingSigns("-", space, setSpace);
-          }}
-        >
-          -
-        </button>
+        <div className="third-in-counterbymyself">
+          <div className="input-space">{space}</div>
+        </div>
+        <div className="btn-in-counterbymyself">
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("7", setSpace, space);
+            }}
+          >
+            7
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("8", setSpace, space);
+            }}
+          >
+            8
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("9", setSpace, space);
+            }}
+          >
+            9
+          </button>
+          <button
+            className="button-in-mainfunc notNumber"
+            onClick={() => {
+              calculatingSigns("/", space, setSpace);
+            }}
+          >
+            /
+          </button>
+          <button className="button-in-mainfunc" onClick={clear}>
+            clear
+          </button>
+        </div>
+        <div className="btn-in-counterbymyself">
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("4", setSpace, space);
+            }}
+          >
+            4
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("5", setSpace, space);
+            }}
+          >
+            5
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("6", setSpace, space);
+            }}
+          >
+            6
+          </button>
+          <button
+            className="button-in-mainfunc notNumber"
+            onClick={() => {
+              calculatingSigns("*", space, setSpace);
+            }}
+          >
+            *
+          </button>
+        </div>
+        <div className="btn-in-counterbymyself">
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("1", setSpace, space);
+            }}
+          >
+            1
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("2", setSpace, space);
+            }}
+          >
+            2
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("3", setSpace, space);
+            }}
+          >
+            3
+          </button>
+          <button
+            className="button-in-mainfunc notNumber"
+            onClick={() => {
+              calculatingSigns("+", space, setSpace);
+            }}
+          >
+            +
+          </button>
+        </div>
+        <div className="btn-in-counterbymyself">
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              calculatingSigns(".", space, setSpace);
+            }}
+          >
+            .
+          </button>
+          <button
+            className="button-in-mainfunc"
+            onClick={() => {
+              btn("0", setSpace, space);
+            }}
+          >
+            0
+          </button>
+          <button className="button-in-mainfunc" onClick={equal}>
+            =
+          </button>
+          <button
+            className="button-in-mainfunc notNumber"
+            onClick={() => {
+              calculatingSigns("-", space, setSpace);
+            }}
+          >
+            -
+          </button>
+        </div>
       </div>
     </div>
   );
